048da04af4103dede7c6aa8a7216781ef26612b0
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-10-15 16:22:05 +1000

    passing dummy data

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+          <UserTable users={users}/>
         </div>
       </div>
     </div>
