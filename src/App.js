c894fcaa63b7e4bb6a33add817580d4182ce2f05
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-10-15 16:20:10 +1000

    dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
 
 const App = () => {
+
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData);
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
